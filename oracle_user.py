918788f70d4003c093d8d55f4729c7f18b3b017e
Thomas Krahn <thomas.krahn@esailors.de>
2015-10-21 17:23:05 +0200

    Ensure create and drop of user as well as changing default tablespace

--- oracle_user.py
+++ oracle_user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 try:
     import cx_Oracle
@@ -12,9 +13,92 @@ def createConnection(username, userpass, host, port, service):
     return cx_Oracle.connect('{username}/{userpass}@{host}:{port}/{service}'.format(username=username, userpass=userpass, host=host, port=port, service=service))
 
 
+def executeSQL(con, sql):
+    cur = con.cursor()
+    try:
+        cur.execute(sql)
+    except cx_Oracle.DatabaseError as e:
+        module.fail_json(msg='{sql}: {err}'.format(sql=sql, err=e))
+    cur.close()
+
+
+def getUser(con, username):
+    user = None
+    cur = con.cursor()
+    cur.prepare(
+        'select default_tablespace,temporary_tablespace,password,account_status from dba_users where username = :username')
+    cur.execute(None, dict(username=username))
+    row = cur.fetchone()
+    if row:
+        user = dict()
+        user['name'] = username
+        user['default_tablespace'] = row[0]
+        user['temporary_tablespace'] = row[1]
+        user['account_status'] = row[3]
+    cur.close()
+    return user
+
+
+def getCreateUserSQL(username, userpass, default_tablespace=None, temporary_tablespace=None, account_status=None):
+    sql = 'CREATE USER "{username}" IDENTIFIED BY "{userpass}"'.format(
+        username=username, userpass=userpass)
+    if default_tablespace:
+        sql = '{sql} DEFAULT TABLESPACE {default_tablespace}'.format(
+            sql=sql, default_tablespace=default_tablespace)
+    if temporary_tablespace:
+        sql = '{sql} TEMPORARY TABLESPACE {temporary_tablespace}'.format(
+            sql=sql, temporary_tablespace=temporary_tablespace)
+    if account_status:
+        sql = '{sql} ACCOUNT {account_status}'.format(
+            sql=sql, account_status=account_status)
+    return sql
+
+
+def getDropUserSQL(username):
+    sql = 'DROP USER "{username}"'.format(username=username)
+    return sql
+
+
+def getUpdateUserSQL(username, userpass=None, default_tablespace=None, temporary_tablespace=None, account_status=None):
+    sql = 'ALTER USER {username}'.format(username=username)
+    if userpass:
+        sql = '{sql} IDENTIFIED BY {userpass}'.format(
+            sql=sql, userpass=userpass)
+    if default_tablespace:
+        sql = '{sql} DEFAULT TABLESPACE {default_tablespace}'.format(
+            sql=sql, default_tablespace=default_tablespace)
+    return sql
+
+
 def ensure():
     name = module.params['name']
     state = module.params['state']
+    user = getUser(conn, name)
+    if not user:
+        if state != 'absent':
+            if state != 'locked':
+                account_status = 'UNLOCK'
+            else:
+                account_status = 'LOCK'
+            sql = getCreateUserSQL(
+                username=name,
+                userpass=module.params['password'],
+                default_tablespace=module.params['default_tablespace'],
+                temporary_tablespace=module.params['temporary_tablespace'],
+                account_status=account_status)
+            executeSQL(conn, sql)
+            return True, getUser(conn, username=name)
+    if user:
+        if state == 'absent':
+            sql = getDropUserSQL(username=name)
+            executeSQL(conn, sql)
+            return True, None
+        if user.get('default_tablespace') != module.params['default_tablespace']:
+            sql = getUpdateUserSQL(
+                username=name, default_tablespace=module.params['default_tablespace'])
+            executeSQL(conn, sql)
+            return True, getUser(conn, username=name)
+    return False, user
 
 
 def main():
@@ -24,6 +108,9 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             name=dict(type='str', required=True),
+            password=dict(type='str', required=False),
+            default_tablespace=dict(type='str', required=False),
+            temporary_tablespace=dict(type='str', required=False),
             state=dict(type='str', default='present', choices=[
                        'present', 'absent', 'locked', 'unlocked']),
             oracle_host=dict(type='str', default='127.0.0.1'),
